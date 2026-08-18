b971ef65b874b58cacf6e4b2e982f4bf0fff1a33
James Hoffman <james-hoffman@live.ca>
2026-08-18 13:20:54 -0400

    wip scenes

--- littlejs/examples/starter/game.js
+++ littlejs/examples/starter/game.js
@@ -12,68 +12,121 @@ function y(value) {
   return WORLD_HEIGHT / WORLD_SCALE * value   
 }
 
+class Scene extends EngineObject {
+  static _name = null
+  static _instance = null 
+
+  static instantiate(name) {
+    const scene = {
+
+      launcher: () => {
+        return new LauncherScene()  
+      },
+
+      game: () => {
+        return new GameScene()  
+      },
+
+    }[name]()
+
+    if (Scene._instance == null) {
+      Scene._name = name
+      Scene._instance = scene
+    }
+
+    return scene
+  }
+
+  static transition(name) {
+    const destination = Scene.instantiate(name)
+
+    Scene._instance.destroy()
+
+    Scene._name = name
+    Scene._instance = destination
+  }
+
+  constructor(color = new Color(0, 0, 0)) {
+      super(vec2(0, 0), vec2(x(100), y(100)), 0, 0, color);
+  }
+}
+
+class LauncherScene extends Scene {
+
+  update() {
+  }
+
+  render() {
+    const ctx = mainContext;
+
+    ctx.save();
+
+    ctx.font = 'bold 160px Arial';
+    ctx.textAlign = 'center';
+
+    const start  = worldToScreen(new vec2(x(0), y(0.25)))
+    const width = 650
+
+    const gradient = ctx.createLinearGradient(start.x - width / 2, start.y, start.x + width / 2, start.y);
+    gradient.addColorStop(0, 'red');
+    gradient.addColorStop(.166, 'orange');
+    gradient.addColorStop(.332, 'yellow');
+    gradient.addColorStop(.498, 'green');
+    gradient.addColorStop(.664, 'blue');
+    gradient.addColorStop(.83, 'indigo');
+    gradient.addColorStop(1, 'violet');
+
+    ctx.fillStyle = gradient;
+    ctx.fillText('Spectrum', start.x, start.y, width);
+
+    ctx.fillStyle = 'white';
+    ctx.fillText('ru', start.x + 129, start.y, width - 510);
+    ctx.fillText('n', start.x + 240, start.y, width - 570);
+
+    ctx.restore();
+  }
+}
+
+class GameScene extends Scene {
+
+  render() {
+    drawCircle(vec2(0,5), 3+oscillate(.5), RED);
+  }
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 // _init
 function gameInit()
 {
   canvasFixedSize = vec2(WORLD_WIDTH, WORLD_HEIGHT);
   cameraScale = WORLD_SCALE;
+
+  Scene.instantiate('launcher')
+  
+  setTimeout(() => {
+    Scene.transition('game')
+  }, 2000)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 // _update
-function gameUpdate()
-{
+function gameUpdate() {
 
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-function gameUpdatePost()
-{
+function gameUpdatePost() {
 
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 // _render
-function gameRender()
-{
-    // draw a grey square in the background without using webgl
-    drawRect(vec2(0,0),vec2(x(100), y(0.5)), new Color(.6,.6,.6), 0, 0);
+function gameRender() {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-function gameRenderPost()
-{
-  const ctx = mainContext;
-
-  ctx.save();
-
-  ctx.font = 'bold 160px Arial';
-  ctx.textAlign = 'center';
-
-  const start  = worldToScreen(new vec2(x(0), y(0.25)))
-  const width = 650
-
-  const gradient = ctx.createLinearGradient(start.x - width / 2, start.y, start.x + width / 2, start.y);
-  gradient.addColorStop(0, 'red');
-  gradient.addColorStop(.166, 'orange');
-  gradient.addColorStop(.332, 'yellow');
-  gradient.addColorStop(.498, 'green');
-  gradient.addColorStop(.664, 'blue');
-  gradient.addColorStop(.83, 'indigo');
-  gradient.addColorStop(1, 'violet');
-
-  ctx.fillStyle = gradient;
-  ctx.fillText('Spectrum', start.x, start.y, width);
-
-  ctx.fillStyle = 'white';
-  ctx.fillText('ru', start.x + 129, start.y, width - 510);
-  ctx.fillText('n', start.x + 240, start.y, width - 570);
-
-  // ctx.fillText('run', start.x + 172, start.y, width - 424);
-
-  ctx.restore();
+function gameRenderPost() {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
